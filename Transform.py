52fa005089e50f954795681b28775c407d91dbcd
Hieu Dang <dangminhhieu781998@gmail.com>
2020-07-29 20:46:52 +0700

    comeback to windows

--- Transform.py
+++ Transform.py
@@ -1,6 +1,8 @@
 #Import
 
+
 import torch
+import random as random
 import torchvision.transforms as transforms
 from torchvision.utils import save_image
 from customDataset import Cards
@@ -11,14 +13,12 @@ my_transforms = transforms.Compose([
 	transforms.ToPILImage(),
 	transforms.Resize((256,256)),
 	transforms.RandomCrop((224,224)),
-	brightness = uniform(0.4,0.6)
-	transforms.ColorJitter(brightness),
-	degree = uniform(0,360)
-	transforms.RandomRotation(degree),
+	transforms.ColorJitter(brightness = 0.5),
+	transforms.RandomRotation(degree = 45),
 	transforms.RandomHorizontalFlip(p=0.5),
 	transforms.RandomVerticalFlip(p=0.05),
 	transforms.RandomGreyScale(p=0.2),
-	transforms.ToTensor()
+	transforms.ToTensor(),
 	transforms.Normalize(mean=[0.0, 0.0, 0.0], std=[1.0,1.0,1.0])
 	])
 
