f11c0f07fd197093ce6e1032487d597cf89d8082
Hieu Dang <dangminhhieu781998@gmail.com>
2020-08-01 13:25:50 +0700

    new rand

--- Transform.py
+++ Transform.py
@@ -2,6 +2,7 @@
 
 
 import torch
+import numpy as np
 import random as random
 import torchvision.transforms as transforms
 from torchvision.utils import save_image
@@ -13,8 +14,8 @@ my_transforms = transforms.Compose([
 	transforms.ToPILImage(),
 	transforms.Resize((256,256)),
 	transforms.RandomCrop((224,224)),
-	transforms.ColorJitter(brightness = uniform(0.4,0.6),contrast = uniform(0.4,0.6),saturation = uniform(0.4,0.6),hue = uniform(0.4,0.6)),
-	transforms.RandomRotation(degree = uniform(0,359)),
+	transforms.ColorJitter(brightness = np.random.uniform(0.4,0.6),contrast = np.random.uniform(0.4,0.6),saturation = np.random.uniform(0.4,0.6),hue = np.random.uniform(0.4,0.6)),
+	transforms.RandomRotation(degree = np.random.uniform(0,359)),
 	transforms.RandomHorizontalFlip(p=0.5),
 	transforms.RandomVerticalFlip(p=0.05),
 	transforms.RandomGreyScale(p=0.2),
@@ -28,4 +29,4 @@ img_num = 0
 for _ in range(50):
 	for im, label in dataset:
 		save_image(img, 'img'+str(ing_num)+'.png')
-		img_num += 1
+		img_num += 1	
